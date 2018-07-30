0dd9a8df7e4d6d3250e31a4250883cba00cec939
YangJiaQi <1295585769@qq.com>
2018-07-30 20:02:53 +0800

    1

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Test : MonoBehaviour {
+
+    private Ray ray;
+    private RaycastHit hit;//射线碰到的碰撞信息  
+    public GameObject navPlayer;//寻路的人  
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = navPlayer.GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //射线起始位置  
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100) && Input.GetMouseButtonDown(0))
+        {
+            agent.SetDestination(hit.point);
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
+        }
+    }  
+}
